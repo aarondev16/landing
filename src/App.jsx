1e4ba732e9c4db585a132f3f9dc68467ceb466b6
aarondev16 <aaronoso0704@gmail.com>
2024-09-04 22:48:31 -0600

    carrusel y navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Nav from "./components/Nav.jsx";
+import Carousel from "./components/Carousel.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
 	return (
 		<div className="h-screen w-screen bg-[#1e1e1e] text-white">
 			<Nav />
+			<Carousel />
+			<Footer />
 		</div>
 	);
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+
+const Footer = () => {
+	return (
+		<footer className="footer">
+			<div className="footer-grid">
+				<ul>
+					<li>Redes Sociales</li>
+					<li>Aaron Campos</li>
+				</ul>
+				<ul>
+					<li>Contacto</li>
+					<li>aaronoso0704@gmail.com</li>
+				</ul>
+				<ul>
+					<li>Escuelas</li>
+					<li>Cecytem Ixtapaluca</li>
+				</ul>
+			</div>
+			<p className="footer-text mt">Realizado por el Dev. Aaron Miranda en FasdaTec</p>
+			<p className="footer-text">Todos los derechos reservados</p>
+		</footer>
+	);
+};
+
+export default Footer;
